b579475b96513383a3462021179b3e928f43d33b
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2026-01-08 02:53:26 -0500

    auto save

--- tests/cpp/tree_test.cpp
+++ tests/cpp/tree_test.cpp
@@ -2,6 +2,7 @@
 #include <dlc_core/tree.h>
 
 TEST(MathTest, LevelOrder) {
+    // TreeNode n15(15), n7(7), n9(9), n20(20, &n15, &n7), root(3, &n9, &n20);
     TreeNode* root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
     EXPECT_EQ(levelOrder(root), {{3}, {9, 20}, {15, 17}});
 }
