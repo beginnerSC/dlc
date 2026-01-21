80358a328455323b13ef7074314204d0c2c8f76f
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2026-01-21 04:06:45 -0500

    auto save

--- dlc_core/src/array.cpp
+++
@@ -1,9 +0,0 @@
-#include "dlc_core/array.h"
-#include <vector>
-#include <ranges>
-#include <functional>
-#include <print>
-
-std::vector<int> productExceptSelf(std::vector<int>& nums) {
-    std::println("{}", std::ranges::fold_left(nums, 1, std::multiplies{}));
-}
